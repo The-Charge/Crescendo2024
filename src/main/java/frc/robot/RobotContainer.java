8f40b4f95f9858374f7c71afd3dbd2a50637163e
ecmason <ecmason@mtu.edu>
2024-02-06 17:57:50 -0500

    Bring over the latest YAGSL changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -93,7 +93,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Path", true);
+    return drivebase.getAutonomousCommand("New Auto");
   }
 
   public void setDriveMode()
